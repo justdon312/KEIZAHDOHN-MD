e5ced1c459fbac8fcad78ace4fd063ffa479d49b
THE PROGRAMMER <142110420+Itxxwasi@users.noreply.github.com>
2024-06-12 08:22:20 +0500

    UNIQUE MD

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -2,6 +2,68 @@ import { exec } from 'child_process'
 import speed from 'performance-now'
 
 let handler = async (m, { conn }) => {
+  let thumbnail = 'https://i.imgur.com/GyAXW62.jpeg'
+  let fgg = {
+    key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' },
+    message: {
+      contactMessage: {
+        displayName: `UNIQUE-MD`,
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:'UNIQUE-MD'\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+      },
+    },
+  }
+  let pingMsg = await conn.sendMessage(m.chat, { text: 'Pinging...' }, { quoted: fgg })
+
+  let timestamp = speed()
+
+  await exec('neofetch --stdout', async (error, stdout) => {
+    let latency = (speed() - timestamp).toFixed(4)
+
+    await conn.relayMessage(
+      m.chat,
+      {
+        protocolMessage: {
+          key: pingMsg.key,
+          type: 14,
+          editedMessage: {
+            conversation: `Pong! Ｂｏｔ ｓｐｅｅｄ: ${latency} ｍｓ`,
+          },
+        },
+      },
+      {}
+    )
+  })
+}
+
+handler.help = ['ping']
+handler.tags = ['main']
+handler.command = ['ping', 'speed']
+
+export default handler
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+
+import { exec } from 'child_process'
+import speed from 'performance-now'
+
+let handler = async (m, { conn }) => {
 
   let pingMsg = await conn.sendMessage(m.chat, {text: 'Pinging...'})
 
@@ -30,3 +92,4 @@ handler.tags = ['main']
 handler.command = ['ping', 'speed', 'pn'] 
 
 export default handler
+**/
