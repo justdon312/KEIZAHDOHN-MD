d457d458e9bfd5f35608e29e4f6c9d91e18a2d6a
justdon312 <dohnbohb@gmail.com>
2024-06-15 19:34:29 +0300

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -3,9 +3,9 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "https://github.com/Itxxwasi/WASI-MD/raw/main/Assets/mp3/alive.mp3"
-    let url = "https://github.com/Itxxwasi/WASI-MD"
-    let murl = "https://youtube.com/@wasitech1"
-    let img = "https://telegra.ph/file/e4e5a6722c1d5bf8ba1a5.jpg"
+    let url = "https://github.com/justdon312/KEIZAHDOHN-MD"
+    let murl = "https://whatsapp.com/channel/0029VahhyIm1noz80Dybyp0a"
+    let img = "https://telegra.ph/file/fa4152408c8d224dc5d3e.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
